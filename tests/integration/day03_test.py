8699db8aba2cd9f6c8d88f7919558e75e10ab65f
Peter Bailey <b33tle@gmail.com>
2020-05-15 15:45:46 -0500

    Fixed test function name

--- tests/integration/day03_test.py
+++ tests/integration/day03_test.py
@@ -18,6 +18,6 @@ def test_part_1(test_case, result):
     ('#1 @ 1,3: 4x4; #2 @ 3,1: 4x4; #3 @ 5,5: 2x2', 3),
     ('#123 @ 3,2: 5x4', 123),
 ])
-def test_part_1(test_case, result):
+def test_part_2(test_case, result):
     file = StringIO(test_case.replace('; ', "\n"))
     assert day03.part2(FileReader(file)) == result
